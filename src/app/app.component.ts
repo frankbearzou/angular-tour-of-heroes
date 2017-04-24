ea2c6f2cb07098b6bf7abaa4c36b1cf5d5daa301
Frank Zou <frankbearzou@gmail.com>
2017-04-23 21:35:07 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Hero} from './hero';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+  hero = new Hero(1, 'Windstorm');
+
 }
---
+++ src/app/hero.ts
@@ -0,0 +1,4 @@
+export class Hero {
+  constructor(private id: number, private name: string) {
+  }
+}
